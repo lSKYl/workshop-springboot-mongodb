e1b4d1fc8a1f5f2ad327fecdc9e3d920d9685f30
Lucas Soares <drak263@gmail.com>
2023-03-30 17:24:59 -0300

    Insercao de usuario com post

--- src/main/java/com/lucas/workshopmongo/services/UserService.java
+++ src/main/java/com/lucas/workshopmongo/services/UserService.java
@@ -7,21 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucas.workshopmongo.domain.User;
+import com.lucas.workshopmongo.dto.UserDTO;
 import com.lucas.workshopmongo.repository.UserRepository;
 import com.lucas.workshopmongo.services.exceptions.ObjectNotFoundException;
+
 //Cuida das regras de negócio
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
