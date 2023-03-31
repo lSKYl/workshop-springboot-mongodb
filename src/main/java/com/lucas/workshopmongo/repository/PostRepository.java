ee17e4b3b2c8f57e68750dd0ab29a75505d56624
Lucas Soares <drak263@gmail.com>
2023-03-31 19:00:33 -0300

    Adicionado comentarios

--- src/main/java/com/lucas/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucas/workshopmongo/repository/PostRepository.java
@@ -9,10 +9,22 @@ import org.springframework.stereotype.Repository;
 
 import com.lucas.workshopmongo.domain.Post;
 
+/*
+ * Referências com query methods:
+https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+ * 
+ * Referências para utilizar Query:
+https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+https://docs.mongodb.com/manual/reference/operator/query/regex/
+ */
+
 //Repository cuida dos comandos como encontrar tal entidade no banco de dados, ou inserir.
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	//Utilizar query para procurar no banco de dados
 	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
 	List<Post> findByTitle(String text);
 	
