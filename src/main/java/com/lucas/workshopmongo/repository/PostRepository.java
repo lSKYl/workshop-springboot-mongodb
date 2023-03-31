50d90a3fd7d1acc4dce801a453048186123ea05a
Lucas Soares <drak263@gmail.com>
2023-03-31 17:38:17 -0300

    Consulta com query methods

--- src/main/java/com/lucas/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucas/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.lucas.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.lucas.workshopmongo.domain.Post;
 //Repository cuida dos comandos como encontrar tal entidade no banco de dados, ou inserir.
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
